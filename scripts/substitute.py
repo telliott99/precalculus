f6aedcf548d3fbe81221dbe6e88002b2b3287257
Tom Elliott <telliott999@gmail.com>
2020-02-03 11:20:10 -0500

    no message

---
+++ scripts/substitute.py
@@ -0,0 +1,22 @@
+import os, util
+
+targ = '$\circ'
+pre = 'circ'
+sub = 'bullet'
+
+d = '/Users/telliott/Github/precalculus/files'
+for fn in os.listdir(d):
+    path = '/'.join([d,fn])
+    sL = list()
+    data = util.load(path)
+    
+    for line in data.strip().split('\n'):
+        if targ in line:
+            if '^' + targ in line:
+                continue
+            line = line.replace(pre,sub)
+        sL.append(line)
+        
+    fh = open(path,'w')
+    fh.write('\n'.join(sL))
+    fh.close()
