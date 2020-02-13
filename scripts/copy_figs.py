22f7e698f469834abc715beb0f1ef4dd0494f05a
Tom Elliott <telliott999@gmail.com>
2020-02-13 12:36:28 -0500

    no message

---
+++ scripts/copy_figs.py
@@ -0,0 +1,22 @@
+import os
+import util
+from shutil import copyfile
+
+opath = '/Users/telliott/Github/precalculus/'
+ipath = '/Users/telliott/Github/calculus_book/'
+
+p = opath + 'files'
+
+#----------------
+
+L = os.listdir(p)
+L.sort()
+
+for fn in L:
+    print(fn)
+    data = util.load(p + '/' + fn)
+    sL = util.get_fig_fn(data)
+    for e in sL:
+        print(e)
+
+
